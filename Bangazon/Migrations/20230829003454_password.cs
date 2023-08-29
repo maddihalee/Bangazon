71486dd802aa77ce66b85fb70a7f483e70b2fb3f
Maddi Cook <maddihcook@gmail.com>
2023-08-28 19:36:06 -0500

    add stuff to user object

---
+++ Bangazon/Migrations/20230829003454_password.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bangazon.Migrations
+{
+    public partial class password : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "Test1234");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatus",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
