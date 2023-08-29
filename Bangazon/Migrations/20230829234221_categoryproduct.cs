e28843edbbd18ffde0a3d135de7bf19bb86588df
Maddi Cook <maddihcook@gmail.com>
2023-08-29 18:42:42 -0500

    migrations update

---
+++ Bangazon/Migrations/20230829234221_categoryproduct.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bangazon.Migrations
+{
+    public partial class categoryproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CategoryProduct",
+                columns: new[] { "CategoriesId", "ProductsId" },
+                values: new object[,]
+                {
+                    { 543, 2 },
+                    { 544, 4 },
+                    { 545, 5 },
+                    { 545, 6 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CategoryProduct",
+                keyColumns: new[] { "CategoriesId", "ProductsId" },
+                keyValues: new object[] { 543, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProduct",
+                keyColumns: new[] { "CategoriesId", "ProductsId" },
+                keyValues: new object[] { 544, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProduct",
+                keyColumns: new[] { "CategoriesId", "ProductsId" },
+                keyValues: new object[] { 545, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryProduct",
+                keyColumns: new[] { "CategoriesId", "ProductsId" },
+                keyValues: new object[] { 545, 6 });
+        }
+    }
+}
